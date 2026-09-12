6cf789fcbb7a35ad08b15c8a226658c98a766e43
nguyennanh9961 <nguyennanh9961@gmail.com>
2026-09-12 11:45:40 +0700

    Add files via upload

---
+++ BT-12-09.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello World!");
+int a;
+int b;
+int c;
+Console.WriteLine("Nhap a:");
+a = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+    Console.WriteLine("Nhap b: ");
+    try
+    {
+        b = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nhap sai, vui long nhap lai");
+    }
+}
+while (true)
+{
+    Console.WriteLine("Nhap c: ");
+    bool k = int.TryParse(Console.ReadLine(), out c);
+    if (k)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Nhap sai, vui long nhap lai");
+    }
+}
+
+Console.WriteLine($"a + b + c = {a + b + c}");
